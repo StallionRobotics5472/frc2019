3bdfa317d050af5f39d031c6bc13538d59fb074f
Manas Mahajan <mahajanm02@gmail.com>
2019-02-13 16:11:51 -0500

    Fixed Unused Imports

--- src/main/java/frc/robot/autonomous/commands/RaiseArmHalf.java
+++ src/main/java/frc/robot/autonomous/commands/RaiseArmHalf.java
@@ -2,21 +2,20 @@ package frc.robot.autonomous.commands;
 
 import frc.robot.Constants;
 import frc.robot.Robot;
-import frc.robot.subsystems.ArmPIDSubsystem;
+
 
 import edu.wpi.first.wpilibj.command.Command;
 
 public class RaiseArmHalf extends Command{
 	
-	private ArmPIDSubsystem arm;
+	
 	boolean isFinished = false;
 	private final double startingPosition = Robot.arm.getPosition();
 	private final double finalPosition = startingPosition + Constants.ARM_HALF_HEIGHT;
 	
 	@Override
 	public void initialize() {
-		arm = Robot.arm;
-		arm.moveArm(0.2);
+		Robot.arm.moveArm(0.2);
 	}
 	
 	@Override
@@ -33,6 +32,6 @@ public class RaiseArmHalf extends Command{
 	@Override
 	public void end()
 	{
-		arm.moveArm(0);
+		Robot.arm.moveArm(0);
 	}
 }
